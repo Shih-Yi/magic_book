72062a18bfbc021b86ce5a7f4ed6896f206aaa54
shihyi <brvast@gmail.com>
2021-12-29 10:39:13 +1300

    add index, update and destroy action

--- app/controllers/user/books_controller.rb
+++ app/controllers/user/books_controller.rb
@@ -1,7 +1,12 @@
 class User::BooksController < User::BaseController
+  before_action :set_book, only: [:edit, :update, :destroy]
 
   def index
-    @books = Book.all
+    @books = current_user.books.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @books.to_json }
+    end
   end
 
   def show
@@ -24,7 +29,22 @@ class User::BooksController < User::BaseController
   def edit
   end
 
+  def update
+    @book.update(book_params)
+    flash[:success] = "Successfully"
+    redirect_to user_books_path
+  end
+
+  def destroy
+    @book.destroy
+    render status: 200, json: { result: '刪除成功' }
+  end
+
   private
+
+  def set_book
+    @book = current_user.books.find(params[:id])
+  end
 
   def book_params
     params.require(:book).permit(:title, :remove_images, :images => [], :attachments_attributes => [:id, :attachment, :description, :_destroy])
