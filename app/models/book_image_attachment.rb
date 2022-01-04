af6e8d1424232f42942eae491336a2ef005b543e
SC <>
2022-01-04 23:46:19 +1300

    check if image under 5MB

--- app/models/book_image_attachment.rb
+++ app/models/book_image_attachment.rb
@@ -3,5 +3,16 @@ class BookImageAttachment < ApplicationRecord
   # relationships .............................................................
   belongs_to :book
 
+  # before_action .............................................................
+  before_save :check_image_size
   # public instance methods ...................................................
+  
+  # private instance methods ..................................................
+  private
+  
+  def check_image_size
+    if self.present? && self.image.file.size > 5120_000
+      raise ActiveRecord::RecordInvalid.new(self)
+    end
+  end
 end
