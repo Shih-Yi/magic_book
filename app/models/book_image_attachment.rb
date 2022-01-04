0cbad4871b147e1752c9d9e1260010c8cae62941
SC <>
2022-01-05 09:33:30 +1300

    add before_save for update_image_attributes

--- app/models/book_image_attachment.rb
+++ app/models/book_image_attachment.rb
@@ -5,6 +5,7 @@ class BookImageAttachment < ApplicationRecord
 
   # before_action .............................................................
   before_save :check_image_size
+  before_save :update_image_attributes
   # public instance methods ...................................................
   
   # private instance methods ..................................................
@@ -13,6 +14,13 @@ class BookImageAttachment < ApplicationRecord
   def check_image_size
     if self.present? && self.image.file.size > 5120_000
       raise ActiveRecord::RecordInvalid.new(self)
+    end
+  end
+
+  def update_image_attributes
+    if image.present? && image_changed?
+      self.image_type = image.file.content_type
+      self.image_size = image.file.size
     end
   end
 end
