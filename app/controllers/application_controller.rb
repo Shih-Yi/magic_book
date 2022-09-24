430176667d9649cd89a8c138dc8618ae495fb156
Shih-Yi <brvast@gmail.com>
2022-09-24 21:42:33 +1200

    add after_sign_in_path_for admin_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    case resource.class.name
+    when 'AdminUser'
+      admin_books_path
+    else
+      root_path
+    end
+  end
 end
