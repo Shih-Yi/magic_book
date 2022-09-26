0eced5ceacf5f5448360bd28b91588386b7fa92d
Shih-Yi <brvast@gmail.com>
2022-09-26 18:44:57 +1300

    change store dir

--- app/uploaders/book_audio_uploader.rb
+++ app/uploaders/book_audio_uploader.rb
@@ -14,7 +14,7 @@ class BookAudioUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/mydata/test"
+    "public/"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
